53619cf47ef30a4a2167a73943d1afb0c90b8bf9
seth spring <seth.c.spring@gmail.com>
2024-01-30 18:35:48 -0500

    phase-1-functions lab complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (distance) {
+    return Math.abs(42 - distance)
+}
+
+function distanceFromHqInFeet (distance) {
+    return Math.abs(42 - distance) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const blockLengthInFeet = 264; 
+    const distanceInBlocks = Math.abs(destination - start); 
+    const distanceInFeet = distanceInBlocks * blockLengthInFeet; 
+    return distanceInFeet;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    let farePrice;
+  
+    if (distanceInFeet <=400) {
+      farePrice = 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      farePrice = (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      farePrice = 25;
+    } else if(distanceInFeet>2500) {
+      farePrice = 'cannot travel that far';
+    }
+  
+    return farePrice;
+  }
